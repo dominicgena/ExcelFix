cba946190be01ae4a942f51c077ed68857116962
Admin <dominic.gena2@gmail.com>
2025-04-02 09:31:23 -0400

    4/2/2025 @ 9:23 AM Next and current lessons are being highlighted the correct color.  The higher end logic plan is to have the autosave program run, which works by detecting an edit and saving after a debounce timer.  The save of the workbook will create a temporary lock file which, when detected, highlight() will be restarted.  This will ensure the lessons array reinitializes, acting on only recent data.  The highlight function works by, on start, resetting the formatting in the cells specified in the "highlight_row" function in highlight_cells.py. Then, the correct cells with the new data are highlighted.

--- src/helper_functions/highlight_next_and_current.py
+++ src/helper_functions/highlight_next_and_current.py
@@ -9,27 +9,38 @@ def highlight():
     for day_lessons in lessons.values():
         for lesson in day_lessons:
             highlight_row(lesson[0], "reset")
+    r = None
+    s = None
     while True:
         current_day, current_time = get_current_day_time()
         next_lesson = get_next_lesson(lessons, current_day, current_time)
         current_lesson = get_current_lesson(lessons, current_day, current_time)
 
         if next_lesson:
-            cls()
+            # cls()
             print(f"Next lesson is: {next_lesson[1]} at {excel_time_to_string(next_lesson[7])} on {next_lesson[3]}")
             # print(f"Next lesson is: {next_lesson[1]} at {next_lesson[7]} on {next_lesson[3]}")
-            # highlight_row(next_lesson[0], "#FF0000")
+            highlight_row(next_lesson[0], "#90EE90") # highlight next lesson light green
+            r = next_lesson[0]
+
         else:
-            cls()
+            highlight_row(r, "reset")
             print("No upcoming lessons.")
 
         if current_lesson:
             print(f"Current lesson is: {current_lesson[1]} at {excel_time_to_string(current_lesson[7])} to {excel_time_to_string(current_lesson[8])}")
             # print(f"Current lesson is: {current_lesson[1]} at {current_lesson[7]} to {current_lesson[8]}")
-
+            highlight_row(current_lesson[0], "#EEE880")
+            s = current_lesson[0]
         else:
+            highlight_row(s, "reset") # huh?
             print("No current lesson is in session.")
 
         time.sleep(5)
 
 highlight()
+# region reset
+# for day_lessons in lessons.values():
+#     for lesson in day_lessons:
+#         highlight_row(lesson[0], "reset")
+# endregion reset
